4ac49e44ed99d52ce81677de3964d1bc8de7aa34
Amit Kumar <guptaajuju1995@gmail.com>
2022-02-17 13:21:16 +0530

    Time: 57 ms (28.47%), Space: 13.6 MB (16.98%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,22 +1,28 @@
-
-
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        stack<pair<char , int>>st;
-    st.push({s[0] , 1});
-    for(int i=1 ; i < s.size() ; i++){
-        if(!st.empty() and st.top().first == s[i])st.top().second++;
-        else st.push({s[i] , 1});
-        if(st.top().second ==k)st.pop();
-    }
-    string ans = "";
-    while(!st.empty()){
-       
-        while(st.top().second--)ans += st.top().first;
-        st.pop();
-    }
-    reverse(ans.begin() , ans.end());
-    return ans;
+        stack<pair<char,int>>st;
+        st.push({s[0],1});
+        for(int i=1;i<s.length();i++){
+            if(!st.empty()&& st.top().first==s[i]){
+                st.push({s[i],st.top().second+1});
+                if(st.top().second==k){
+                    while(!st.empty() && st.top().first==s[i]) st.pop();
+                }
+            }
+            else{
+                  st.push({s[i],1});
+                if(st.top().second==k){
+                    while(!st.empty() && st.top().first==s[i]) st.pop();
+                }
+            }
+        }
+       s="";
+        while(!st.empty()){
+           s+=st.top().first;
+            st.pop();
+        }
+        reverse(s.begin(),s.end());
+        return s;
     }
 };
