ecf866f8cba77f5d6d9aaacf58cec0e6a46c7f6a
Amit Kumar <guptaajuju1995@gmail.com>
2022-03-02 14:49:02 +0530

    Time: 51 ms (64.94%), Space: 17.7 MB (75.53%) - LeetHub

--- 143-reorder-list/143-reorder-list.cpp
+++ 143-reorder-list/143-reorder-list.cpp
@@ -8,29 +8,91 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
+// {solution 1ST using ARRAY}
+
+
+//  void reorderList(ListNode* head) {
+//       vector<int>arr;
+//         ListNode* curr=head;
+//         while(curr){
+//             arr.push_back(curr->val);
+//             curr=curr->next;
+//         }
+//         int i=0,j=arr.size()-1;
+//         int ind=0;
+//         while(head){
+//            if(ind==0)
+//            {
+//                head->val=arr[i++];
+//            }else{
+//                head->val=arr[j--];
+//            }
+//             ind=!ind;
+//             head=head->next;
+            
+//         }
+        
+        
+//     }
+
+
+
+
+
 class Solution {
 public:
-    void reorderList(ListNode* head) {
-      vector<int>arr;
-        ListNode* curr=head;
+    ListNode* reverse(ListNode* head){
+        if(head==NULL)
+            return NULL;
+        ListNode* prev=NULL,*curr=head;
         while(curr){
-            arr.push_back(curr->val);
-            curr=curr->next;
+            ListNode* nextNode=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nextNode;
         }
-        int i=0,j=arr.size()-1;
-        int ind=0;
-        while(head){
-           if(ind==0)
-           {
-               head->val=arr[i++];
-           }else{
-               head->val=arr[j--];
-           }
-            ind=!ind;
-            head=head->next;
+        return prev;
+    }
+    ListNode* findMid(ListNode* head){
+        ListNode* slow=head;
+        ListNode* fast=head;
+        ListNode* prev;
+        while(slow && fast && fast->next){
+            prev=slow;
+            slow=slow->next;
+            fast=fast->next->next;
             
         }
+        prev->next=NULL;
+        return slow;
         
+    }
+    void reorderList(ListNode* head) {
+          if(head==NULL||head->next==NULL||head->next->next==NULL)
+            return ;
+        //find the middle of list
+        ListNode* slow=head,*fast=head->next;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* secondHead=slow->next;
+        slow->next=NULL;
+        //reverse the second list
+        secondHead=reverse(secondHead);
+        ListNode* curr1=head,*curr2=secondHead;
         
+        //linking list
+        while(curr1 && curr2){
+            ListNode* temp1=curr1->next;
+            ListNode* temp2=curr2->next;
+            curr1->next=curr2;
+            curr2->next=temp1;
+            curr1=temp1;
+            curr2=temp2;
+        }
+     
     }
 };
