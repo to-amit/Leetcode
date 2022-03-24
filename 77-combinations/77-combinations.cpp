0a936a39e237baa77b873b2ae6be227c195bdeea
Amit Kumar <guptaajuju1995@gmail.com>
2022-03-24 10:52:07 +0530

    Time: 901 ms (5.13%), Space: 165.1 MB (9.41%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-  void powerset(int val, int n, int k, vector<int>& present, vector<vector<int>> &ans) {
-	if(k == present.size()) {
-		ans.push_back(present);
-		return;
-	}
-
-	if(val > n){
-		return;
-	}
-
-	powerset(val+1, n, k, present, ans);
-	present.push_back(val);
-	powerset(val+1, n, k, present, ans);
-	present.pop_back();
-}
-
-vector<vector<int>> combine(int n, int k) {
-
-	vector<vector<int>> ans;
-	vector<int> present;
-	powerset(1, n, k, present, ans);
-	return ans;
-}
-};
+ vector<vector<int>> ans;
+    
+    void helper(int n, int k, vector<int> temp, int index)
+    {
+        if(temp.size() == k)        // Base case: since we want size = k
+        {
+            ans.push_back(temp);
+            return;
+        }
+        
+        for(int i=index; i<=n; i++)
+        {
+            temp.push_back(i);          // Add the element to current vector to backtrack later
+            helper(n, k, temp, i+1);
+            temp.pop_back();            // Backtrack
+        }
+    }
+    
+    vector<vector<int>> combine(int n, int k) 
+    {
+        vector<int> temp;
+        helper(n, k, temp, 1);
+		
+        return ans;
+    }
+    
+}; 
