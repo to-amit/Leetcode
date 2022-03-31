b299095416d7343f04e73b0e56741fdd202a4d78
Amit Kumar <guptaajuju1995@gmail.com>
2022-03-31 14:49:09 +0530

    Time: 11 ms (54.43%), Space: 11 MB (91.76%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -8,41 +8,42 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-//  ListNode* deleteDuplicates(ListNode* head) {
-//  ListNode * temp = new ListNode(0); 
-//         temp->next=head;
-//         ListNode * prev = temp;  
-        
-//         while(head) 
-//         {
-//             if(head->next && head->val == head->next->val)
-//             {
-//                 while(head->next  && head->val == head->next->val)
-//                 {
-//                     head = head->next;
-//                 }
-//                 prev->next = head->next; 
-// 				}
-//             else
-//                 prev = prev ->next; 
-//             head= head->next;
+//     ListNode* deleteDuplicates(ListNode* head) {
+//         if(!head || !head->next) return head;
+
+//         if (head->next->val != head->val) {
+//             head->next = deleteDuplicates(head->next);
 //         }
-//         return temp->next;
+//         else {
+//             while (head->next && head->next->val == head->val)
+//                 head = head->next;
+//             return deleteDuplicates(head->next);
+//         }
+        
+//         return head;
 //     }
+
 class Solution {
 public:
-    ListNode* deleteDuplicates(ListNode* head) {
-        if(!head || !head->next) return head;
-
-        if (head->next->val != head->val) {
-            head->next = deleteDuplicates(head->next);
-        }
-        else {
-            while (head->next && head->next->val == head->val)
-                head = head->next;
-            return deleteDuplicates(head->next);
-        }
+   ListNode* deleteDuplicates(ListNode* head) {
+ ListNode * temp = new ListNode(0); 
+        temp->next=head;
+        ListNode * prev = temp;  
         
-        return head;
+        while(head) 
+        {
+            if(head->next && head->val == head->next->val)
+            {
+                while(head->next  && head->val == head->next->val)
+                {
+                    head = head->next;
+                }
+                prev->next = head->next; 
+				}
+            else
+                prev = prev ->next; 
+            head= head->next;
+        }
+        return temp->next;
     }
 };
