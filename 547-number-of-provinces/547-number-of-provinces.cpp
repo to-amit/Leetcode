1d65dccfed05222aa4c57aed70e1279763769880
Amit Kumar <guptaajuju1995@gmail.com>
2022-04-13 10:25:16 +0530

    Time: 23 ms (85.91%), Space: 13.8 MB (62.94%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,41 +1,25 @@
 class Solution {
 public:
-   
+    
+    
     int findCircleNum(vector<vector<int>>& isConnected) {
-       int count =0;
-       int n =isConnected.size();
-       vector<int>adj[n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1){
-                    adj[i].push_back(j);
-                }
-            }
-        }
-        vector<int>vis(n,-1);
-        for(int i=0;i<n;i++){
-            if(vis[i]==-1){
-                
-                count++;
-                queue<int>q;
-                q.push(i);
-                vis[i]=1;
-                
-                while(!q.empty()){
-                    int temp=q.front();
-                    q.pop();
-                    vis[temp] =1;
-                    for(auto it :adj[temp]){
-                        if(vis[it]==-1){
-                            vis[it]=1;
-                            q.push(it);
-                        }
-                    }
-                    
-                }
-                
-            }
-        }
-        return count;
-    }
+     
+		int n=isConnected.size(),cnt=0;
+		vector<bool> vis(n,false);
+		for(int i=0;i<n;i++){
+			if(!vis[i]){
+				cnt++;
+				dfs(isConnected,i,vis);
+			}
+		}
+		return cnt;
+	}
+	void dfs(vector<vector<int>>& graph,int st,vector<bool> &vis){
+		vis[st]=true;
+		for(int i=0;i<graph[st].size();i++){
+			if(graph[st][i]==1 && !vis[i])  dfs(graph,i,vis);
+		}
+	}
+        
+    
 };
