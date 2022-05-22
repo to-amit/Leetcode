e11cc75ca671e6bdab314e2b0dd6c66b98219abc
Amit Kumar <guptaajuju1995@gmail.com>
2022-05-22 11:10:25 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (5.00%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    int find_util(int m,int n , vector<vector<int>>&dp){
-    
-    
-    if(m==0 and n==0) return 1;
-    
-    if(m<0 or n<0) return 0;
-    
-    
-    if(dp[m][n]!=-1) return dp[m][n];
-    
-    
-    
-    int up = find_util(m-1,n,dp);
-    int left = find_util(m,n-1,dp);
-    
-    
-    return dp[m][n]=left+up;
-    
-    
-}
+    int numberOfPaths(int m, int n,vector<vector<int>>&dp)
+    {
+       
+        if (m == 1 || n == 1)
+            return dp[m][n]=1;
+        if(dp[m][n]!=-1)return dp[m][n];
+        
+       return dp[m][n]= numberOfPaths(m - 1, n,dp) + numberOfPaths(m, n - 1,dp);
+     
+    }
     int uniquePaths(int m, int n) {
-       vector<vector<int>>dp (m,vector<int>(n,-1));
-     
-     
-     
-     return find_util(m-1,n-1,dp);        
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return numberOfPaths(m,n,dp);
+        
     }
 };
